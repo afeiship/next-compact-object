91b7db6f659ef89a2376f425d3dc92384e7922ae
afeiship <afeiship@gmail.com>
2019-01-17 21:06:10 +0800

    feat: migrate deep

--- dist/next-compact-object.js
+++ dist/next-compact-object.js
@@ -1,11 +1,26 @@
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
+  var isArray = function(target) {
+    return target instanceof Array;
+  };
 
   nx.compactObject = function(inObject) {
     var result = {};
     nx.forIn(inObject, function(key, value) {
-      value && (result[key] = value);
+      if (value !== null && typeof value === 'object') {
+        if (isArray(value)) {
+          var arrayValue = [];
+          nx.forEach(value, function(vValue) {
+            arrayValue.push(nx.compactObject(vValue));
+          });
+          result[key] = arrayValue;
+        } else {
+          result[key] = nx.compactObject(value);
+        }
+      } else {
+        value && (result[key] = value);
+      }
     });
     return result;
   };
