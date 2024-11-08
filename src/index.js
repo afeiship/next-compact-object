2ea14e4db7dc6ddaebe318ea98b01e258be50e67
afeiship <1290657123@qq.com>
2024-11-08 08:47:55 +0800

    fix: is empty nil object

--- src/index.js
+++ src/index.js
@@ -3,26 +3,34 @@ import nx from '@jswork/next';
 const isEmptyDefault = (value) => value == null;
 const isObj = (target) => target !== null && typeof target === 'object';
 
-nx.compactObject = function (inObject, inIsEmpty) {
+nx.compactObject = function(inObject, inIsEmpty) {
   const result = {};
   const isArrayEmpty = Array.isArray(inIsEmpty) ? (v) => inIsEmpty.includes(v) : inIsEmpty;
   const isEmpty = isArrayEmpty || isEmptyDefault;
+  const getNestedValue = function(obj) {
+    const isAry = Array.isArray(obj);
+    if (!isAry) {
+      return nx.compactObject(obj, isEmpty);
+    } else {
+      return nx.map(obj, function(_, subValue) {
+        return isObj(subValue) ? nx.compactObject(subValue, isEmpty) : subValue;
+      });
+    }
+  };
 
-  nx.forIn(inObject, function (key, value) {
+  nx.forIn(inObject, function(key, value) {
     if (isObj(value)) {
       if (!isEmpty(value)) {
-        const isAry = Array.isArray(value);
-        result[key] = !isAry
-          ? nx.compactObject(value, isEmpty)
-          : nx.map(value, function (_, vValue) {
-              const isobj = typeof vValue === 'object';
-              return isobj ? nx.compactObject(vValue, isEmpty) : vValue;
-            });
+        const nestedValue = getNestedValue(value);
+        if (!isEmpty(nestedValue)) {
+          result[key] = nestedValue;
+        }
       }
     } else {
       !isEmpty(value) && (result[key] = value);
     }
   });
+
   return result;
 };
 
